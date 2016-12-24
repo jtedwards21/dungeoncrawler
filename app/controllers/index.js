b015755fe5049153117a4ea17141f41d25ea8d5c
James Ahlm <14782828428@qq.com>
2016-12-24 11:51:05 +0800

    Initial Commit

---
+++ app/controllers/index.js
@@ -0,0 +1,21 @@
+var Crawler = React.createClass({
+  getInitialState() {
+    return {
+　　　　　　
+    };
+
+  },
+  render(){
+　　　　return(
+　　　　　　<h1>
+	Hello World
+      </h1>
+    )
+  }
+})
+
+
+ReactDOM.render(
+  <Crawler  />,
+  document.getElementById('container')
+)
