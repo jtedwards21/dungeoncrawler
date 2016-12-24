20a494702f740157ca7351238a2040acb727a2dd
James Ahlm <14782828428@qq.com>
2016-12-24 12:06:43 +0800

    Crawler

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,15 +1,45 @@
+
+var weapons = [{name: "dog", attack: 2}, {name: "brass knuckles", attack: 10}, {name: "pistol", attack: "20"}]
+
 var Crawler = React.createClass({
   getInitialState() {
     return {
-　　　　　　
+　　　　　　health: 100,
+      level: 1,
+      weapon: {name:"dog",attack:2},
+      xp: 0
     };
-
+  },
+  changeWeapon(name){
+    var weapon = {};
+    for(var i = 0;i < weapons.length;i++){
+	if(weapons[i].name == name){weapon = weapons[i]}
+    }
+    this.setState({weapon:weapon});
   },
   render(){
 　　　　return(
 　　　　　　<h1>
 	Hello World
       </h1>
+    )
+  }
+})
+
+
+var InfoBox = React.createClass({
+  getInitialState(){
+    return {};
+  },
+  render(){
+    return(
+      <div>
+	{this.props.weapon.name}
+	{this.props.weapon.attack}
+	{this.props.level}
+	{this.props.health}
+	{this.props.xp}
+      </div>
     )
   }
 })
