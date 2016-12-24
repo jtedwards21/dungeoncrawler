21981c78a2e53255c10449b69b35cefc2dbe4c51
James Ahlm <14782828428@qq.com>
2016-12-24 13:38:17 +0800

    More Classes

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -7,15 +7,36 @@ var Crawler = React.createClass({
 　　　　　　health: 100,
       level: 1,
       weapon: {name:"dog",attack:2},
-      xp: 0
+      xp: 0,
+      enemies: [],
+      items: []
     };
   },
+  generateMap(){
+
+  },
+  generateEnemies(){
+
+  },
+  generateItems(){
+
+  }
   changeWeapon(name){
     var weapon = {};
     for(var i = 0;i < weapons.length;i++){
 	if(weapons[i].name == name){weapon = weapons[i]}
     }
     this.setState({weapon:weapon});
+  },
+　　changeHealth(changeValue){
+    var health = this.state.health;
+    this.setState({health: health + changeValue});
+  },
+  changeXp(changeValue){
+    var Xp = this.state.xp;
+    var level = this.state.level;
+    Xp = xp + changeValue;
+    this.setState({xp:Xp%100, level: level+(Math.floor(Xp/100))})
   },
   render(){
 　　　　return(
@@ -26,6 +47,52 @@ var Crawler = React.createClass({
   }
 })
 
+var WeaponItem = React.createClass({
+  getInitialState(){
+    return {
+	name: this.props.name,
+	attack: this.props.attack
+    };
+  },
+  render(){
+    return (
+      <div></div>
+    )
+  }
+})
+
+var HealthItem = React.createClass({
+  getInitialState(){
+    return {
+	value: this.props.value
+    };
+  },
+  render(){
+    return (
+	<div></div>
+    )
+  }
+})
+
+var Enemy = React.createClass({
+  getInitialState(){
+    return {
+	      level: 1,
+	      health: 100
+	   };
+  },
+  getAttackValue(){
+    var minimum = Math.ceil(this.state.level * 4);
+    var maximum = Math.floor(this.state.level * 5);
+    return Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+  },
+  render(){
+    return (
+	<div>
+	</div>
+    )
+  }
+})
 
 var InfoBox = React.createClass({
   getInitialState(){
